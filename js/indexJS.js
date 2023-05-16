59e6698480a43356581607da6f3225338eeae66b
Aman Sharma <mail.aman@protonmail.com>
2023-05-16 15:46:43 +0400

    UI updates

--- js/indexJS.js
+++ js/indexJS.js
@@ -1,34 +1,51 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    let currencySelectorFrom = document.getElementById("FromInputA");
-    let currencySelectorTo = document.getElementById("ToInputA");
-    let fromIMG = document.getElementById("FromIMG");
-    let toIMG = document.getElementById("ToIMG");
+    let currencySelectorFrom = document.getElementsByClassName("darkenIMG")[0];
+    let currencySelectorTo = document.getElementsByClassName("darkenIMG")[1];
+    let fromInput = document.getElementById("FromInputA");
+    let toInput = document.getElementById("ToInputA");
+    let fromIMG = document.getElementById("FromIMG")
     fetch('https://openexchangerates.org/api/currencies.json')
         .then(response => response.json())
         .then(currencies => {
             for(let currency in currencies){
-                const option = document.createElement('option');
-                option.value = currency;
-                option.text = currencies[currency];
+                const option = document.createElement('div');
+                option.setAttribute("data-value", currency);
+                option.className = "OptionsFrom"
+                option.textContent = currencies[currency];
                 currencySelectorFrom.appendChild(option);
+                option.addEventListener("mousedown", ()=>{
+                    let countryCode = option.getAttribute("data-value").toLowerCase().slice(0, 2);
+                    fromInput.value = option.getAttribute("data-value");
+                    fromIMG.style.backgroundImage = "url(https://flagcdn.com/w2560/" + countryCode +".png)"
+                })
+                option.addEventListener("mousemove", ()=>{
+                    option.style.fontSize = "10vh";
+                })
+                option.addEventListener("mouseout", ()=>{
+                    option.style.fontSize = "7vh";
+                })
 
-                const optionTo = document.createElement('option')
-                optionTo.value = currency;
-                optionTo.text = currencies[currency];
+                const optionTo = document.createElement('div');
+                optionTo.setAttribute("data-value", currency)
+                optionTo.className = "OptionsTo"
+                optionTo.textContent = currencies[currency];
                 currencySelectorTo.appendChild(optionTo);
+                optionTo.addEventListener("mousedown", ()=>{
+                    toInput.value = optionTo.getAttribute("data-value");
+                })
+                optionTo.addEventListener("mousemove", ()=>{
+                    optionTo.style.fontSize = "10vh";
+                })
+                optionTo.addEventListener("mouseout", ()=>{
+                    optionTo.style.fontSize = "7vh";
+                })
             }
         }).catch(error => {
-        console.error(error)
+            console.error(error)
     })
-    currencySelectorFrom.addEventListener("change", () => {
-        let countryCode = currencySelectorFrom.value.toLowerCase().slice(0, 2);
-        fromIMG.style.backgroundImage = "url(https://flagcdn.com/w2560/" + countryCode +".png)"
-        console.log("async")
-    })
-    currencySelectorTo.addEventListener("change", () => {
-        let countryCode = currencySelectorTo.value.toLowerCase().slice(0, 2);
-        toIMG.style.backgroundImage = "url(https://flagcdn.com/w2560/" + countryCode +".png)"
-        console.log("async")
-    })
-
+    // currencySelectorFrom.addEventListener("change", () => {
+    //     let countryCode = currencySelectorFrom.value.toLowerCase().slice(0, 2);
+    //     fromIMG.style.backgroundImage = "url(https://flagcdn.com/w2560/" + countryCode +".png)"
+    //     console.log("async")
+    // })
 })
